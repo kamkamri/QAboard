9d89e62b7479e2a957aa3cd5871c2f5a32d6f04e
EC2 Default User <ec2-user@ip-172-31-19-174.ap-northeast-1.compute.internal>
2022-10-23 06:08:44 +0000

    [Add]Gmail設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,6 +96,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # gmail設定
+  config.action_mailer.default_url_options = {  host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    domain: 'gmail.com',
+    port: 587,
+    user_name: Rails.application.credentials.gmail[:user_name],  #Gmailアドレス（credentials.yml.encに記載）
+    password: Rails.application.credentials.gmail[:password],  #アプリパスワード（credentials.yml.encに記載）
+    authentication: :login
+  }
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
